0fa6bca594d0c2bcaf0cebe54f7d0f1b4307aa36
JulienKisoni <kisonijulien@gmail.com>
2020-04-01 11:21:46 +0100

    initial commit

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,12 +1,8 @@
-import * as React from 'react';
-import Hello from './Hello';
-import Info from './Info';
+import React from 'react';
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
   </div>
 );
 
